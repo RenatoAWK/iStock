68a4cc7042f5b4963092b089bc4dc9e54ae4ca24
Renato Ferreira Germano <Renatoawk@gmail.com>
2018-11-16 17:05:54 -0200

    Tela de Cadastro de empresa

--- app/src/main/java/bsi/mpoo/istock/CadastroActivity.java
+++ app/src/main/java/bsi/mpoo/istock/CadastroActivity.java
@@ -6,21 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cadastro);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
 
     }
-    // Teste de design
-    public void criarConta(View view) {
-        Intent intent = new Intent(this, CadastroActivity.class);
+
+    //teste de design
+    public void cadastrar(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
     //
