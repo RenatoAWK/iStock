1eeeedee53351eafe8ee3680e7656f994b73971f
Bruno Santos <brunosantos@protonmail.com>
2018-11-26 16:33:36 -0300

    getters and setters

--- app/src/main/java/bsi/mpoo/istock/domain/User.java
+++ app/src/main/java/bsi/mpoo/istock/domain/User.java
@@ -7,4 +7,52 @@ public class User {
     private String type;
     private String status;
     private String company;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
 }
