cd7ef27a305c7523283efdf13839796799668045
Renato Ferreira Germano <Renatoawk@gmail.com>
2018-11-26 17:37:30 -0200

    UserDAO +addUser

--- app/src/main/java/bsi/mpoo/istock/data/UserDAO.java
+++ app/src/main/java/bsi/mpoo/istock/data/UserDAO.java
@@ -1,8 +1,33 @@
 package bsi.mpoo.istock.data;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.Context;
 
+import bsi.mpoo.istock.domain.User;
+
 public class UserDAO{
+    private Context context;
+    public UserDAO(Context context){
+        this.context = context;
+    }
+
+    Database mDbHelper = new Database(context);
+
+    public void addUser(User user) throws Exception {
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(ContractUser.COLUMN_NAME, user.getName());
+        values.put(ContractUser.COLUMN_EMAIL, user.getEmail());
+        values.put(ContractUser.COLUMN_PASSWORD, user.getPassword());
+
+        long newRowID = db.insert(ContractUser.TABLE_NAME, null, values);
+
+        user.setId(newRowID);
+    }
+
+
 
 }
