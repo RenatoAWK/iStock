49af366466f571fd9fbe4d835ac7ccae2269ca9d
Bruno Santos <brunosantos@protonmail.com>
2018-11-26 16:23:47 -0300

    add class user

---
+++ app/src/main/java/bsi/mpoo/istock/domain/user.java
@@ -0,0 +1,10 @@
+package bsi.mpoo.istock.domain;
+
+public class user {
+    private long id;
+    private String email;
+    private String password;
+    private String type;
+    private String status;
+    private String company;
+}
