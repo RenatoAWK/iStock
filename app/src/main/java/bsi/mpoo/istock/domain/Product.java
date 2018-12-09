e0644d3e767d40a4422fc41f51185971177e81e9
Renato Ferreira Germano <Renatoawk@gmail.com>
2018-12-09 14:27:38 -0200

    Adição da classe de domínio Produto com parcel

---
+++ app/src/main/java/bsi/mpoo/istock/domain/Product.java
@@ -0,0 +1,88 @@
+package bsi.mpoo.istock.domain;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.math.BigDecimal;
+
+public class Product implements Parcelable {
+
+    private long id;
+    private String name;
+    private BigDecimal price;
+    private long minimumQuantity;
+    private User user;
+
+    public Product(){}
+
+    public Product(Parcel parcel){
+
+        this.id = parcel.readLong();
+        this.name = parcel.readString();
+        this.price = new BigDecimal(parcel.readString());
+        this.minimumQuantity = parcel.readLong();
+        this.user = (User)parcel.readValue(User.class.getClassLoader());
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public long getMinimumQuantity() {
+        return minimumQuantity;
+    }
+
+    public void setMinimumQuantity(long minimumQuantity) {
+        this.minimumQuantity = minimumQuantity;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeLong(id);
+        dest.writeString(name);
+        dest.writeString(price.toString());
+        dest.writeLong(minimumQuantity);
+        dest.writeValue(user);
+
+    }
+
+    public static final Parcelable.Creator<Product> CREATOR = new Parcelable.Creator<Product>(){
+
+
+        @Override
+        public Product createFromParcel(Parcel source) {
+            return new Product(source);
+        }
+
+        @Override
+        public Product[] newArray(int size) {
+            return new Product[size];
+        }
+    };
+}
